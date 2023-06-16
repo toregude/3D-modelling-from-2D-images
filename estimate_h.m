e546e0c73ae60ea0d51f7ae33bf09509b7819280
Pedermannen1 <pstokkan@outlook.com>
2023-06-16 10:04:21 +0200

    H-matrise
    La til en måte å finne Homografimatrisa

--- estimate_h.m
+++ estimate_h.m
@@ -1,6 +1,6 @@
 %% Tores feature matching
-I1 = im2gray(imread("DSC_0682.JPG"));
-I2 = im2gray(imread("DSC_0684.JPG"));
+I1 = im2gray(imread("DSC_0680.JPG"));
+I2 = im2gray(imread("DSC_0681.JPG"));
 
 points1 = detectHarrisFeatures(I1);
 points2 = detectHarrisFeatures(I2);
@@ -13,4 +13,8 @@ indexPairs = matchFeatures(features1,features2);
 matchedPoints1 = valid_points1(indexPairs(:,1),:);
 matchedPoints2 = valid_points2(indexPairs(:,2),:);
 
-showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
+%showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
+
+[tform,inlierpoints1,inlierpoints2] = estimateGeometricTransform(matchedPoints1,matchedPoints2,'projective')
+disp('Homography Matrix:');
+disp(tform.T');
