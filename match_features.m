b666747de77622814ced413057b9f00e6ad4d3b2
Terje <terjjac@stud.ntnu.no>
2023-06-23 15:25:20 +0200

    Changes to be committed: 	deleted:    kodekodekode.m 	modified:   match_features.m 	deleted:    untitled.asv

--- match_features.m
+++ match_features.m
@@ -1,34 +1,32 @@
-I1 = im2gray(imread("DSC_0682.JPG"));
-cameraParams = cameraParameters;
-I2 = im2gray(imread("DSC_0684.JPG"));
-
-K = [3408.59 0 3117.24; 0 3408.87 2064.07; 0 0 1];
-
-points1 = detectHarrisFeatures(I1);
-points2 = detectHarrisFeatures(I2);
-
-[features1,valid_points1] = extractFeatures(I1,points1);
-[features2,valid_points2] = extractFeatures(I2,points2);
-
-indexPairs = matchFeatures(features1,features2);
-
-matchedPoints1 = valid_points1(indexPairs(:,1),:);
-matchedPoints2 = valid_points2(indexPairs(:,2),:);
-
-
-
-
-[E,inliers] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2,Method="RANSAC", NumTrials=2000,DistanceThreshold=1e-3);
-
-matchedPoints1_inliers = matchedPoints1(inliers==1);
-matchedPoints2_inliers = matchedPoints2(inliers==1);
-correspondences = [matchedPoints1_inliers.Location, matchedPoints2_inliers.Location];
-figure; 
-showMatchedFeatures(I1,I2,matchedPoints1_inliers,matchedPoints2_inliers);
-correspondences'
-
-[T1, T2, R1, R2, U, V] = TR_from_E(E);
-[T, R, lambda, P1, camC1, camC2] = reconstruction(T1, T2, R1, R2, correspondences', K)
+function [E, matchedPoints1_inliers,matchedPoints2_inliers] = match_features(I1, I2)
+    I1 = im2gray(imread("DSC_0682.JPG"));
+    cameraParams = cameraParameters;
+    I2 = im2gray(imread("DSC_0684.JPG"));
+    
+    K = [3408.59 0 3117.24; 0 3408.87 2064.07; 0 0 1];
+    
+    points1 = detectHarrisFeatures(I1);
+    points2 = detectHarrisFeatures(I2);
+    
+    [features1,valid_points1] = extractFeatures(I1,points1);
+    [features2,valid_points2] = extractFeatures(I2,points2);
+    
+    indexPairs = matchFeatures(features1,features2);
+    
+    matchedPoints1 = valid_points1(indexPairs(:,1),:);
+    matchedPoints2 = valid_points2(indexPairs(:,2),:);
+    
+    [E,inliers] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2,Method="RANSAC", NumTrials=2000,DistanceThreshold=1e-3);
+    
+    matchedPoints1_inliers = matchedPoints1(inliers==1);
+    matchedPoints2_inliers = matchedPoints2(inliers==1);
+    correspondences = [matchedPoints1_inliers.Location, matchedPoints2_inliers.Location];
+    figure; 
+    showMatchedFeatures(I1,I2,matchedPoints1_inliers,matchedPoints2_inliers);
+    correspondences'
+    
+    [T1, T2, R1, R2, U, V] = TR_from_E(E);
+    [T, R, lambda, P1, camC1, camC2] = reconstruction(T1, T2, R1, R2, correspondences', K)
 
 %%  Functions
 
@@ -161,28 +159,4 @@ function [T, R, lambda, P1, camC1, camC2] = reconstruction(T1, T2, R1, R2, corre
     ylabel('y');
     zlabel('z');
     grid on;
-end
-
-=======
-function [E, matchedPoints1_inliers,matchedPoints2_inliers] = match_features(I1, I2)
-    
-    points1 = detectHarrisFeatures(I1);
-    points2 = detectHarrisFeatures(I2);
-    
-    [features1,valid_points1] = extractFeatures(I1,points1);
-    [features2,valid_points2] = extractFeatures(I2,points2);
-    
-    indexPairs = matchFeatures(features1,features2);
-    
-    matchedPoints1 = valid_points1(indexPairs(:,1),:);
-    matchedPoints2 = valid_points2(indexPairs(:,2),:);
-    
-    [E,inliers] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2,Method="RANSAC", NumTrials=2000,DistanceThreshold=1e-3);
-    
-    matchedPoints1_inliers = matchedPoints1(inliers==1);
-    matchedPoints2_inliers = matchedPoints2(inliers==1);
-    
-    figure; 
-    showMatchedFeatures(I1,I2,matchedPoints1_inliers,matchedPoints2_inliers);
-
 end
