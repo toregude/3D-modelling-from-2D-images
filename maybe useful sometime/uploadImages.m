f5a5ad809b315975ef7a0943af4fde5503c7a2d0
toregude <gude.tore@gmail.com>
2023-06-30 13:59:10 +0200

    GUI to upload pictures

--- maybe useful sometime/uploadImages.m
+++ maybe useful sometime/uploadImages.m
@@ -1,26 +1,38 @@
-% Display a dialog box to choose multiple image files
-[fileNames, path] = uigetfile({'*.jpg;*.jpeg;*.png;*.bmp'}, 'Select image files', 'MultiSelect', 'on');
-if isequal(fileNames, 0)
-    disp('No image files selected.')
-    return;
-end
+% appdesigner('GUI.mlapp');
 
-% Determine the number of selected images
-numImages = numel(fileNames);
 
-% Initialize a cell array to store the image data
-imageDataArray = cell(1, numImages);
+% Open the GUI
+app = GUI;
 
-% Read and store the image data for each selected image
-for i = 1:numImages
-    imagePath = fullfile(path, fileNames{i});
-    imageDataArray{i} = imread(imagePath);
-end
+% Wait for the GUI to close
+waitfor(app.UIFigure);
 
-% Display the uploaded images
-figure;
-for i = 1:numImages
-    subplot(1, numImages, i);
-    imshow(imageDataArray{i});
-    title(['Image ', num2str(i)]);
-end
+X = load('imageData.mat');
+images = X.imageDataArray;
+% 
+% % Display a dialog box to choose multiple image files
+% [fileNames, path] = uigetfile({'*.jpg;*.jpeg;*.png;*.bmp'}, 'Select image files', 'MultiSelect', 'on');
+% if isequal(fileNames, 0)
+%     disp('No image files selected.')
+%     return;
+% end
+% 
+% % Determine the number of selected images
+% numImages = numel(fileNames);
+% 
+% % Initialize a cell array to store the image data
+% imageDataArray = cell(1, numImages);
+% 
+% % Read and store the image data for each selected image
+% for i = 1:numImages
+%     imagePath = fullfile(path, fileNames{i});
+%     imageDataArray{i} = imread(imagePath);
+% end
+% 
+% % Display the uploaded images
+% figure;
+% for i = 1:numImages
+%     subplot(1, numImages, i);
+%     imshow(imageDataArray{i});
+%     title(['Image ', num2str(i)]);
+% end
