e0a11e97f094467bd08d9697fa43474eacfafbc9
toregude <gude.tore@gmail.com>
2023-07-05 11:14:05 +0200

    Made into functions

--- cv_functions/toreTry3Dcordinates.m
+++ cv_functions/toreTry3Dcordinates.m
@@ -30,49 +30,69 @@ focalLength = [K(1,1) K(2,2)];
 principalPoint = [K(1,3) K(2,3)];
 intrinsics = cameraIntrinsics(focalLength,principalPoint,imageSize);
 
-features = cell(1,size(imageFiles,1));
-valid = cell(1,size(imageFiles,1));
 
-%%
-for i = 1:size(imageFiles,1)
-    I1 = im2gray(imread(imageFiles{i}));
-    harrisFeatures1 = detectHarrisFeatures(I1);
-    [features1,validCorners1] = extractFeatures(I1,harrisFeatures1);
-    features{i} = features1;
-    valid{i} = validCorners1;
-end
-%%
-tree = cell(1,size(imageFiles,1));
-matches = cell(size(imageFiles,1),size(imageFiles,1));
-for i = 1:size(imageFiles,1)
-    for j = i+1:size(imageFiles,1)
-        feature1 = features{i};
-        feature2 = features{j};
-        [indexPairs,matchMetric] = matchFeatures(feature1.Features,feature2.Features);
-        val1 = valid{i};
-        val2 = valid{j};
-        matchedPoints1 = val1.Location(indexPairs(:,1),:);
-        matchedPoints2 = val2.Location(indexPairs(:,2),:);
-        if length(matchedPoints1)>5 && length(matchedPoints2)>5
-            tree{i}(end+1) = j;
-            tree{j}(end+1) = i;
-            matches{i,j} = matchedPoints1;
-            matches{j,i} = matchedPoints2;
-        end
-    end
-end
-%%
+%% Her er koden du trenger for å kjøre, alt over er bare lagt til for å få det til å kjøre
+[features,valid] = find_features(imageFiles);
+[tree, matches] = find_match_tree(features,valid, imageFiles);
 sequence = findCompletedSequence(tree, 1);
 disp(sequence);
+%Bare sortering av bildene og kameraposisjonene, sortering av bilder tror
+%jeg ikke trengs
 imageFiles = imageFiles(sequence);
 cam_pos_sorted = cam_pos_sorted(sequence);
 
 Es = cell(1,size(imageFiles,1)-1);
 inlierPoints1 = cell(1,size(imageFiles,1)-1);
 inlierPoints2 = cell(1,size(imageFiles,1)-1);
+
+%Akkurat hva som må gjøres her vet jeg ikke. Per nå lagres 
+%alle Eene og inliersa i en cell, men kanskje bedre å direkte
+%her regne ut 3D koordinater?
 for i = 1:size(imageFiles,1)-2 % Usikker på hvorfor det må være -2 og ikke -1, virker som at siste bildet ikke blir sortert
     [E, epipolarInliers] = estimateEssentialMatrix(matches{sequence(i),sequence(i+1)}, matches{sequence(i+1),sequence(i)}, intrinsics, Confidence = 0.99);
+    
+    matchPoints1 = matches{sequence(i),sequence(i+1)};
+    matchPoints2 = matches{sequence(i+1),sequence(i)};
+    
+    %Regn ut 3D kordinater
     Es{i} = E;
     inlierPoints1{i} = matches{sequence(i),sequence(i+1)}(epipolarInliers, :);
     inlierPoints2{i} = matches{sequence(i+1),sequence(i)}(epipolarInliers, :);
 end
+
+%% 
+% 
+% 
+% 
+% 
+% Below here is code needed to sort the images
+% features = cell(1,size(imageFiles,1));
+% valid = cell(1,size(imageFiles,1));
+% for i = 1:size(imageFiles,1)
+%     I1 = im2gray(imread(imageFiles{i}));
+%     harrisFeatures1 = detectHarrisFeatures(I1);
+%     [features1,validCorners1] = extractFeatures(I1,harrisFeatures1);
+%     features{i} = features1;
+%     valid{i} = validCorners1;
+% end
+% %%
+% tree = cell(1,size(imageFiles,1));
+% matches = cell(size(imageFiles,1),size(imageFiles,1));
+% for i = 1:size(imageFiles,1)
+%     for j = i+1:size(imageFiles,1)
+%         feature1 = features{i};
+%         feature2 = features{j};
+%         [indexPairs,matchMetric] = matchFeatures(feature1.Features,feature2.Features);
+%         val1 = valid{i};
+%         val2 = valid{j};
+%         matchedPoints1 = val1.Location(indexPairs(:,1),:);
+%         matchedPoints2 = val2.Location(indexPairs(:,2),:);
+%         if length(matchedPoints1)>5 && length(matchedPoints2)>5
+%             tree{i}(end+1) = j;
+%             tree{j}(end+1) = i;
+%             matches{i,j} = matchedPoints1;
+%             matches{j,i} = matchedPoints2;
+%         end
+%     end
+% end
+%%
