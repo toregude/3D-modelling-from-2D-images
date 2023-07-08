7225603862579d83869bfddf6b1d540c73198000
toregude <gude.tore@gmail.com>
2023-07-08 20:01:39 +0200

    Minor cleanup in function find feature

--- cv_functions/find_features.m
+++ cv_functions/find_features.m
@@ -4,9 +4,6 @@ function [features, valid_points] = find_features(imageFiles,intrinsics)
     imageSize = size(im2gray(imread(imageFiles{1})));
     for i = 1:size(imageFiles,1)
         I1 = im2gray(undistortImage(imread(imageFiles{i}),intrinsics));
-        if i == 5
-            I1 = I1';
-        end
         I1 = imresize(I1, imageSize);
         harrisFeatures1 = detectHarrisFeatures(I1);
         [features1,validCorners1] = extractFeatures(I1,harrisFeatures1);
