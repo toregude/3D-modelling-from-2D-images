18b760aa4557d811e1326c009eb3c017e8f0ec53
toregude <gude.tore@gmail.com>
2023-07-09 11:33:36 +0200

    Need to merge

--- main.m
+++ main.m
@@ -15,23 +15,17 @@ end
 imageFiles = app.imageDataArray;
 
 %Calibration matrix for the kicker
-K = K_from_cameraparams(app.camerastxt);
+intrinsics = intrinsics_from_cameraparams(camerastxt, imageFiles);
 
 %%Denen koden skal i utgangspunktet ikke trenges, men trengs på et eller
 %%annet sykt vis, ikke rør
 % [cam_pos_sorted,sort_index] = sortrows(app.imagestxt);
 % imageFiles = imageFiles(sort_index);
 
-imageSize = size(im2gray(imread(imageFiles{1})));
-focalLength = [K(1,1) K(2,2)];
-principalPoint = [K(1,3) K(2,3)];
-intrinsics = cameraIntrinsics(focalLength,principalPoint,imageSize);
-
 %% Her er koden du trenger for å kjøre, alt over er bare lagt til for å få det til å kjøre
 [features, valid_points] = find_features(imageFiles,intrinsics);
 %% 
 [G, matches] = find_match_graph(features, valid_points, imageFiles);
-% sequence = dfsearch(G,1)';
 
 %%
 scale_factor = find_scale_factor(app.imagestxt);
