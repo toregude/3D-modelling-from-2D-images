f146619af1cf0350dab46d61f905ba11d4c73f2b
toregude <gude.tore@gmail.com>
2023-07-09 13:28:25 +0200

    Minor fix

--- cv_functions/intrinsics_from_cameraparams.m
+++ cv_functions/intrinsics_from_cameraparams.m
@@ -5,12 +5,10 @@ function intrinsics = intrinsics_from_cameraparams(camerastxt, imageFiles)
     cy = camerastxt(3,8);
 
     K = [fx 0 cx; 0 fy cy; 0 0 1];
-
-
+    
     imageSize = size(im2gray(imread(imageFiles{1})));
     focalLength = [K(1,1) K(2,2)];
     principalPoint = [K(1,3) K(2,3)];
     intrinsics = cameraIntrinsics(focalLength,principalPoint,imageSize);
-
 end
 
