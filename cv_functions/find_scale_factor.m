1c986fe69e52b9b741f64f889f4a96ff06b62d0f
toregude <gude.tore@gmail.com>
2023-07-09 14:17:46 +0200

    Scale factor as matrix

--- cv_functions/find_scale_factor.m
+++ cv_functions/find_scale_factor.m
@@ -1,16 +1,16 @@
 function scale_factor = find_scale_factor(allData, matches)
-
-    
-    try
-        campos_seq = allData(:, 2:4);
-        scale_factor = zeros(size(allData,1)-1,1);
-        for i = 1:size(allData,1)-1
-            scale_factor(i) = (abs(campos_seq(i)-campos_seq(i+1)));
+    scale_factor = zeros(size(matches)); 
+    for i = 1:size(matches,1)
+        for j = 1:size(matches,2)
+            if ~isempty(matches{i,j})
+                try
+                    scale_factor(i,j) = (norm(allData(i, 2:4)-allData(j, 2:4)));
+                catch
+                    scale_factor(i,j) = 1;
+                end
+            else
+                scale_factor(i,j) = 0;
+            end
         end
-    catch
-        scale_factor = ones(size(allData,1)-1,1);
-    end
-    if isnan(scale_factor)
-        scale_factor = ones(size(allData,1)-1,1);
     end
 end
