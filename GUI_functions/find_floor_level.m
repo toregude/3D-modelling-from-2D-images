c3465a2f2dfdb7a821167d3ac092422b4795246e
Terje <terjjac@stud.ntnu.no>
2023-07-11 16:13:37 +0200

    Code is finished

--- GUI_functions/find_floor_level.m
+++ GUI_functions/find_floor_level.m
@@ -1,17 +1,24 @@
-function floor_level = find_floor_level(data)
-    minimum_floor = -10.;
-    maximum_floor = 10.;
+function floor_level = find_floor_level(points_3D_array)
+    
+    %Setting parameters to arbitrary levels
+    minimum_floor = -10;
+    maximum_floor = 10;
+
+    %Dividing the z-axis in intervals of 5 centimeter.
     delta_z = 0.05;
     z = minimum_floor:delta_z:maximum_floor;
     last_count = 0;
     floor_level = minimum_floor;
+
+    %Loop to decide floor level. If we have more than 20 points in one z-level, and the gradient is bigger than 5, we have the floor level.
     for i = 1:(length(z)-1)
-        count = length(data(data(:, 3) < z(i+1))) - length(data(data(:, 3) <= z(i)));
+        count = length(points_3D_array(points_3D_array(:, 3) < z(i+1))) - length(points_3D_array(points_3D_array(:, 3) <= z(i)));
         if count < 20
             last_count = count;
             continue;
         else
-            if count/last_count > 5
+            gradient = count/last_count;
+            if gradient > 5
                 floor_level = z(i);
                 break;
             end
--- GUI_functions/find_roof_level.m
+++ GUI_functions/find_roof_level.m
@@ -1,18 +1,25 @@
-function roof_level = find_roof_level(data)
-    minimum_roof = -10.;
-    maximum_roof = 10.;
+function roof_level = find_roof_level(points_3D_array)
+    
+    %Setting parameters to arbitrary levels
+    minimum_roof = -30.;
+    maximum_roof = 30.;
+
+    %Dividing the z-axis in intervals of 5 centimeter.
     delta_z = 0.05;
     z = minimum_roof:delta_z:maximum_roof;
     z = sort(z, 'descend');
     last_count = 0;
     roof_level = minimum_roof;
+    
+    %Loop to decide roof level. If we have more than 20 points in one z-level, and the gradient is bigger than 5, we have the roof level.
     for i = 1:(length(z)-1)
-        count = length(data(data(:, 3) > z(i+1))) - length(data(data(:, 3) >= z(i)));
+        count = length(points_3D_array(points_3D_array(:, 3) > z(i+1))) - length(points_3D_array(points_3D_array(:, 3) >= z(i)));
         if count < 20
             last_count = count;
             continue;
         else
-            if count/last_count > 5
+            gradient = count/last_count;
+            if gradient > 5
                 roof_level = z(i);
                 break;
             end
